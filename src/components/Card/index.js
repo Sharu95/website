1ad8a6b2706009b4976f2d09195c04e18c689aa7
Sharu K <Sharu@1x-193-157-185-251.uio.no>
2017-12-15 16:18:44 +0100

    refactor: styles
    fix styles aga again and the card hover bug

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,11 +13,11 @@ class Card extends Component<*, *> {
 		console.log('Project', this.props.content);
 
     return (
-      <div className="card-item">
+			<div className="card-item">
 				<div className="card-title"> {this.props.content.title} </div>
 				<div className="card-divider" />
 				<div className="card-description"> {this.props.content.description} </div>
-      </div>
+			</div>
     );
   }
 }
